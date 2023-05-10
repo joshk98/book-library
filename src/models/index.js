f2a08efad826ba65e7fb5138aaab83a8e243988f
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-05-10 22:13:16 +0100

    Created models > index.js

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+const { PGDATABASE, PGUSER, PGPASSWORD, PGHOST, PGPORT } = process.env;
+
+const setupDatabase = () => {
+  const connection = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+    host: PGHOST,
+    port: PGPORT,
+    dialect: "postgres",
+    logging: false,
+  });
+
+  connection.sync({ alter: true });
+  return {};
+};
+
+module.exports = setupDatabase();
