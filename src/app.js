7d7a17cab0246b6020cb1e47a359acf5dd8adf44
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-05-16 22:41:03 +0100

    CRUD reader tests

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const readerController = require("../src/controllers/reader");
 
 const app = express();
+
 app.use(express.json());
 
-app.post("/readers", readerController.createReader);
+app.post("/readers", readerController.create);
+app.get("/readers", readerController.findAll);
+app.get("/readers/:id", readerController.findByPk);
+app.patch("/readers/:id", readerController.update);
+app.delete("/readers/:id", readerController.delete);
 
 module.exports = app;
--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -1,6 +1,35 @@
 const { Reader } = require("../models");
 
-exports.createReader = async (req, res) => {
-  const newReader = await Reader.create(req.body);
-  res.status(201).json(newReader);
+exports.create = async (req, res) => {
+  res.status(201).json(await Reader.create(req.body));
+};
+
+exports.findAll = async (_req, res) => {
+  res.status(200).json(await Reader.findAll());
+};
+
+exports.findByPk = async (req, res) => {
+  const reader = await Reader.findByPk(req.params.id);
+  if (!reader) {
+    return res.status(404).json({ error: "The reader could not be found." });
+  }
+  res.status(200).json(reader);
+};
+
+exports.update = async (req, res) => {
+  const [reader] = await Reader.update(req.body, {
+    where: { id: req.params.id },
+  });
+  if (!reader) {
+    return res.status(404).json({ error: "The reader could not be found." });
+  }
+  res.status(200).json(reader);
+};
+
+exports.delete = async (req, res) => {
+  const reader = await Reader.destroy({ where: { id: req.params.id } });
+  if (!reader) {
+    return res.status(404).json({ error: "The reader could not be found." });
+  }
+  res.status(204).json(reader);
 };
